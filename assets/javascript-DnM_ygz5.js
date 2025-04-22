e22279d7d5b07dbbb86c053257f4b6766ab41e1b
Richard Gazdik <richard.gazdik+github@gmail.com>
2025-04-22 17:18:48 +0200

    feat: Update to new version

--- assets/javascript-DnM_ygz5.js
+++ assets/javascript-DnM_ygz5.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-CQW8WC0S.js";import"./index-BL42UHXf.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-B6dcg_q7.js";import"./index-XnTFmIq9.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
