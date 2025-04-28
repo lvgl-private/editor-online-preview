8ee651d34d380a8c60f353a5169fa828690657bf
Richard Gazdik <richard.gazdik+github@gmail.com>
2025-04-28 14:42:51 +0200

    feat: Various smaller UI tweaks

--- assets/javascript-C01ZWd3e.js
+++ assets/javascript-C01ZWd3e.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-B6dcg_q7.js";import"./index-XnTFmIq9.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-CHHXTie7.js";import"./index-Do05T4A-.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
